e5e087a5b5e2f7b30157dd3f7fd6f42f5df75155
Leogelv <monxydd@gmail.com>
2025-02-06 14:37:32 +0800

    fix: update next.config.js for Railway

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['*'],
+      bodySizeLimit: '50mb'
+    },
   },
   webpack: (config) => {
     config.resolve.fallback = {
